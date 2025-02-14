b47eafc853c56fc7fba4adf2ee90e580ec4d2c00
Agung Shandy <Call Center@Shandy.emtekgroup.id>
2025-02-14 17:11:50 +0700

    registrasi form update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Register from './register'
 import Logo from './assets/Logo.png'
 import IlustrasiLogin from './assets/Illustrasi Login.png'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,15 +14,10 @@ function App() {
             <div className='simsLogo'>
               <img src={Logo} alt="logo" />SIMS PPOB
             </div>
-            <h1 className='text-xl'>Vite + React</h1>
+            <h1 className='text-xl'></h1>
             <Register/>
             <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-              </button>
-              <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-              </p>
+              <p></p>
             </div>
             <p></p>
         </div>
--- src/Register.jsx
+++ src/Register.jsx
@@ -7,8 +7,9 @@
          <input type="text" name="namaBelakang" id="namaBelakang" placeholder="Nama Belakang" required/>
          <input type="password" name="buatPass" id="buatPass" placeholder="Buat Password" required/>
          <input type="password" name="konfirmPass" id="konfirmPass" placeholder="Konfirmasi Password" required/>
-         <input className="btn btn-blue" type="button" value="Registrasi" />
-         <button type="submit">Registrasi</button>
+         <button className="rounded-md bg-red-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
+         Registrasi
+         </button>
       </div>
     </div>
  )   
