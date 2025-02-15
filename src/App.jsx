ab23f8f216c57e8b9dbdba3f6df30d885895fd1e
agungshndy <agungshandy43@gmail.com>
2025-02-15 12:19:18 +0700

    apps update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
 // import { useState } from 'react'
-import Register from './register'
-import Logo from './assets/Logo.png'
-import IlustrasiLogin from './assets/Illustrasi Login.png'
+import Register from './components/Register'
 import './index.css'
 
 function App() {
 
   return (
-    <>
     <div className='container mx-auto'>
-      <div className='grid grid-cols-2'>
-        <div className='register'>
-            <div className='simsLogo'>
-              <img src={Logo} alt="logo" />SIMS PPOB
-            </div>
-            <h1 className='text-xl'></h1>
-            <Register/>
-            <div className="card">
-              <p></p>
-            </div>
-            <p></p>
-        </div>
-        <div className='logo'>
-          <img src={IlustrasiLogin} alt="Ilustrasi Login" />
-        </div>
-      </div>
+      <Register/>
     </div>
-    </>
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,11 @@
- function Register() {
+import Logo from '../assets/Logo.png'
+import IlustrasiLogin from '../assets/Illustrasi Login.png'
+
+function Register() {
  return (
-    <div>
-      <div className="registerForm">
+    <div className='grid grid-cols-2'>
+      <div className="registerForm w-3xs">
+         <img src={Logo} alt="logo" />SIMS PPOB
          <input type="email" name="email" id="email" placeholder="Masukkan E-mail Anda" required/>
          <input type="text" name="namaDepan" id="namaDepan" placeholder="Nama Depan" required/>
          <input type="text" name="namaBelakang" id="namaBelakang" placeholder="Nama Belakang" required/>
@@ -10,6 +14,10 @@
          <button className="rounded-md bg-red-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
          Registrasi
          </button>
+         <p className="text-sm">Sudah punya akun? Login <a href="">di sini</a></p>
+      </div>
+      <div className='logo'>
+          <img src={IlustrasiLogin} alt="Ilustrasi Login" />
       </div>
     </div>
  )   
