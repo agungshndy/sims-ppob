b573053134b394ad3bd0a648b0f58e35fbf483d0
Agung Shandy <Call Center@Shandy.emtekgroup.id>
2025-02-17 14:56:56 +0700

    transaction page update

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -51,7 +51,7 @@ function Transaction() {
         </div>
     <div className='grid grid-cols-1'>
         <input className='border border-gray-200 p-2 my-5 rounded-sm' type="text" name="nominal" id="nominal" placeholder='10.000'/> 
-        <button className="hover:cursor-pointer rounded-md bg-red-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+        <button className="hover:cursor-pointer font-semibold rounded-md bg-red-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-red-700 focus:shadow-none active:bg-red-700 hover:bg-red-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
             Bayar
         </button> 
     </div>  
