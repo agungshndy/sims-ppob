b901e50211cbffe6a058e5cd449b75c41f25ce0c
Agung Shandy <Call Center@Shandy.emtekgroup.id>
2025-02-21 14:08:04 +0700

    add transaction detail page

---
+++ Pages/TransactionDetailPage.jsx
@@ -0,0 +1,8 @@
+import TransactionDetail from "../src/components/TransactionDetail";
+
+const TransactionDetailPage = ()=> {
+    return <TransactionDetail/>
+
+}
+
+export default TransactionDetailPage
